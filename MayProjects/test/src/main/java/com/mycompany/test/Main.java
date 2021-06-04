fba10b5de353e4abc650455c2d401a5dc65a2394
Rainbow49 <n_khmelev@mail.ru>
2021-06-04 18:25:44 +0900

    PacMan

---
+++ MayProjects/test/src/main/java/com/mycompany/test/Main.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.test;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!!");
+        
+        test New = new test();
+        
+        System.out.println("New is"+ New.getA());
+    }
+    
+}
---
+++ MayProjects/test/src/main/java/com/mycompany/test/test.java
@@ -0,0 +1,26 @@
+
+package com.mycompany.test;
+
+
+public class test {
+    int a;
+
+    public test (){
+        this.a=0;
+    }
+    
+    public test(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+    
+    
+    
+}
